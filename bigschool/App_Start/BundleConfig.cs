81c972cc005a9e91c28d106244276cc176b0158b
Khang Nguyen <khang8131398@gmail.com>
2020-06-08 22:50:55 +0700

    Update

--- bigschool/App_Start/BundleConfig.cs
+++ bigschool/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace bigschool
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
--- bigschool/Controllers/HomeController.cs
+++ bigschool/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace bigschool.Controllers
             return View();
         }
 
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
